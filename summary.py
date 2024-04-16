ebc5537431e646375751bfad3315c0b6a9e01d88
yura1234 <yura_1234@mail.ru>
2024-04-16 22:09:43 +0300

    fixed summary class

--- summary.py
+++ summary.py
@@ -2,7 +2,7 @@ import requests
 import json
 import ast
 from bs4 import BeautifulSoup as bs
-from deep_translator import GoogleTranslator
+# from deep_translator import GoogleTranslator
 
 class Summary:
     
@@ -11,42 +11,33 @@ class Summary:
         self.summaryURL = 'https://api.aicloud.sbercloud.ru/public/v2/summarizator/predict'
         self.searchURL = f'https://www.last.fm/ru/music/{artistName}/+wiki'
 
-        self.__findOk = True
 
-        self.__page = bs(requests.get(self.searchURL).text)
+    def loadDescription(self) -> str:
+        page = bs(requests.get(self.searchURL).text)
 
-        self.tranlatedText = self.__makeQuotes(self.__page.find("div", class_="wiki-content").text[:1000])
+        if 'У нас пока нет вики-статьи об этом исполнителе' not in page.text:
+            return self.__makeQuotes(page.find("div", class_="wiki-content").text[:1000])
 
-        # self.tranlatedText = self.__makeQuotes(self.__page.find("div", class_="wiki-content").text[:100])
+        return ''
 
-        # if 'We don\'t have a wiki for this artist.' not in self.__page.text: 
-        #     self.__findOk = True
-        #     self.__sourceText = self.__page.find("div", class_="wiki-content").text[:1000]
-        #     self.__translator = GoogleTranslator(source='en', target='ru')
-
-        #     self.tranlatedText = self.__makeQuotes(self.__translator.translate(self.__sourceText))
-    
 
     def __makeQuotes(self, text: str) -> str:
         return '\"' + text + '\"'
-        # if text[0] != '\"':
-        #     text[0] = '\"'
-        # if text[len(text) - 1] != '\"':
-        #     text[len(text) - 1] = '\"'
 
 
     def makeSummary(self) -> str:
-        if self.__findOk == False:
+        desc = self.loadDescription()
+
+        if not desc:
             return ''
 
         headers = {
-            'accept': 'application/json',
-            'Content-Type': 'application/json',
+            "accept": "application/json",
+            "Content-Type": "application/json",
         }
         data = {
             "instances": [{
-                # "text" : self.tranlatedText[:(int)(len(self.tranlatedText) / 3)] + '\"',
-                "text" : self.tranlatedText,
+                "text" : desc,
                 "num_beams": 10,
                 "num_return_sequences": 20,
                 "length_penalty": 2.0
@@ -54,14 +45,9 @@ class Summary:
         }
 
         jsonData = json.dumps(ast.literal_eval(str(data)))
-        print(jsonData)
         response = requests.post(self.summaryURL, headers=headers, data=str(jsonData)).json()
 
         if response['comment'] == 'Ok!':
             return response['prediction_best']['bertscore']
         else:
             return ''
-        
-
-if __name__ == '__main__':
-    print(Summary('Prodigy').makeSummary())
